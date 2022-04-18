2b8d8c71e8cc42613dedd1ecc12f8032e5c1e804
luissssssssssssssxd <98342890+luissssssssssssssxd@users.noreply.github.com>
2022-04-18 19:26:18 -0400

    orden

--- cly-forms/cly-forms/src/main/java/com/sistema/apirest/ClyFormsApplication.java
+++ cly-forms/cly-forms/src/main/java/com/sistema/apirest/ClyFormsApplication.java
@@ -1,6 +1,5 @@
 package com.sistema.apirest;
 
-import java.util.Iterator;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
--- cly-forms/cly-forms/src/main/java/com/sistema/apirest/Controller/UrgenciaRestController.java
+++ cly-forms/cly-forms/src/main/java/com/sistema/apirest/Controller/UrgenciaRestController.java
@@ -1,10 +1,5 @@
 package com.sistema.apirest.Controller;
 
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.util.ArrayList;
-import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -13,14 +8,11 @@ import java.util.stream.Collectors;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.io.Resource;
 import org.springframework.dao.DataAccessException;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.validation.BindingResult;
-import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -29,12 +21,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.sistema.apirest.Service.IUrgenciaService;
-import com.sistema.apirest.Service.UrgenciaServiceImpl;
 import com.sistema.apirest.entity.Urgencia;
 
 @CrossOrigin(origins = { "http://localhost:4200"})
