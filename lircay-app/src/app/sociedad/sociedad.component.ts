81949de06bcb6e5eb158123af3ae2f4e128a318f
luissssssssssssssxd <98342890+luissssssssssssssxd@users.noreply.github.com>
2022-04-25 19:16:00 -0400

    carpetas front

---
+++ lircay-app/src/app/sociedad/sociedad.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sociedad',
+  templateUrl: './sociedad.component.html',
+  styleUrls: ['./sociedad.component.css']
+})
+export class SociedadComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
